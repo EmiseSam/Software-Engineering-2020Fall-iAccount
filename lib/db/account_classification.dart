91a13ea4f311332079bce5a36231cbeb5140ad9e
EmiseSam <71053373+EmiseSam@users.noreply.github.com>
2020-10-27 03:11:08 +0800

    update new version

--- lib/db/account_classification.dart
+++ lib/db/account_classification.dart
@@ -1,16 +1,19 @@
 import 'package:i_account/db/column.dart';
+import 'column.dart';
 
 class AccountClassification extends Object {
   int id;
   String account;
   double sum;
   double balance;
+  int typeofA;
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
       columnAccount: account,
       columnMoney: sum,
-      columnBalance: balance
+      columnBalance: balance,
+      columntypeofA: typeofA
     };
     if (id != null) {
       map[columnId] = id;
@@ -18,12 +21,13 @@ class AccountClassification extends Object {
     return map;
   }
 
-  AccountClassification(String account,
+  AccountClassification(String account, int typeofA,
       {int id, double sum = 0.00, double balance = 0.00}) {
     this.id = id;
     this.account = account;
     this.sum = sum;
     this.balance = balance;
+    this.typeofA = typeofA;
   }
 
   AccountClassification.fromMap(Map<String, dynamic> map) {
@@ -31,5 +35,6 @@ class AccountClassification extends Object {
     account = map[columnAccount];
     sum = map[columnMoney];
     balance = map[columnBalance];
+    typeofA = map[columntypeofA];
   }
 }
--- lib/db/db_helper_demo.dart
+++ lib/db/db_helper_demo.dart
@@ -7,10 +7,9 @@ import 'package:sqflite/sqflite.dart';
 import 'package:i_account/db/column.dart';
 import 'package:i_account/db/account_classification.dart';
 import 'package:flutter/services.dart';
-
 import 'column.dart';
 
-var dbHelp = new DBHelper();
+var dbAccount = new DBHelper();
 
 class DBHelper {
   //单例
@@ -23,11 +22,11 @@ class DBHelper {
   //获取数据库
   Future<Database> get db async {
     if (_db != null) {
-      print("成功获取数据库");
+      print("成功获取账户数据库");
       return _db;
     }
     _db = await _initDb();
-    print("成功创建数据库");
+    print("成功创建账户数据库");
     return _db;
   }
 
@@ -48,50 +47,117 @@ class DBHelper {
       $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
       $columnAccount TEXT NOT NULL,
       $columnMoney REAL,
-      $columnBalance REAL
+      $columnBalance REAL,
+      $columntypeofA INTEGER
     )
     """;
     await db.execute(queryStringAccount);
   }
 
-  //插入或者更新账户类型（已完成）
+  //插入或者更新账户（已完成）
   Future<int> insertAccount(AccountClassification ac) async {
+    print('正在更新账户');
     var dbClient = await db;
     var result;
+    List<Map> maps = await dbClient.query(tableAccount, columns: [
+      columnId,
+      columnAccount,
+      columnMoney,
+      columnBalance,
+      columntypeofA
+    ]);
+    List<AccountClassification> accounts = new List();
+    for (int i = 0; i < maps.length; i++) {
+      accounts.add(AccountClassification.fromMap(maps[i]));
+    }
+    List templist = new List();
+    accounts.forEach((element) {
+      templist.add(element.account);
+    });
     try {
-      if (ac.id == null) {
-        result = await dbClient.insert(tableAccount, ac.toMap());
-      } else {
+      if (ac.id != null) {
         result = await dbClient.update(tableAccount, ac.toMap(),
             where: '$columnId = ?', whereArgs: [ac.id]);
+        print('已更新账户');
+      } else if (templist.contains(ac.account)) {
+        List<Map> map = await dbClient.query(tableAccount,
+            columns: [
+              columnId,
+              columnAccount,
+              columnMoney,
+              columnBalance,
+              columntypeofA
+            ],
+            where: '$columnAccount',
+            whereArgs: [ac.account]);
+        result = AccountClassification.fromMap(map.first).id;
+        print('已有同名账户');
+      } else {
+        result = await dbClient.insert(tableAccount, ac.toMap());
+        print('成功添加账户');
       }
     } catch (e) {
       debugPrint(e.toString());
     }
     return result;
+
+    // try {
+    //   if (ac.id == null) {
+    //     result = await dbClient.insert(tableAccount, ac.toMap());
+    //   } else {
+    //     result = await dbClient.update(tableAccount, ac.toMap(),
+    //         where: '$columnId = ?', whereArgs: [ac.id]);
+    //   }
+    // } catch (e) {
+    //   debugPrint(e.toString());
+    // }
+    // return result;
   }
 
   //删除账户（已完成）
-  Future<int> deleteAccount(int id) async {
+  Future<int> deleteAccount(String account) async {
+    print('正在删除账户');
     var dbClient = await db;
-    return await dbClient
-        .delete(tableAccount, where: '$columnId = ?', whereArgs: [id]);
+    var result = await dbClient.delete(tableAccount,
+        where: '$columnAccount = ?', whereArgs: [account]);
+    return result;
   }
 
-  //获取账户类型列表（已完成）
+  //获取账户列表（已完成）
   Future<List> getAccountList() async {
+    print('正在获取账户列表');
     var dbClient = await db;
-    List<Map> maps =
-        await dbClient.query(tableMember, columns: [columnId, columnMember]);
-    List<AccountClassification> accounts = [];
+    List<Map> maps = await dbClient.query(tableAccount, columns: [
+      columnId,
+      columnAccount,
+      columnMoney,
+      columnBalance,
+      columntypeofA
+    ]);
+    List<AccountClassification> accounts = new List();
     for (int i = 0; i < maps.length; i++) {
       accounts.add(AccountClassification.fromMap(maps[i]));
     }
     return accounts;
   }
 
+  //查询某一种类的所有账户
+  Future<List> getAccounts(int typeofA) async {
+    var dbClient = await db;
+    var maps = await dbClient.rawQuery(
+        "SELECT * FROM $tableAccount WHERE $columntypeofA = $typeofA");
+    List<AccountClassification> accounts = new List();
+    print('正在查询$typeofA 类账户');
+    for (int i = 0; i < maps.length; i++) {
+      accounts.add(AccountClassification.fromMap(maps[i]));
+      print(accounts[i].account);
+    }
+    return accounts;
+  }
+
   //查询某个账户的总金额（已完成）
   Future<double> accountSum(int id) async {
+    print('正在计算$id 个账户的总金额');
     var dbClient = await db;
     var maps = await dbClient.rawQuery(
         "SELECT $columnBalance FROM $tableAccount where: $columnId = $id");
@@ -101,6 +167,7 @@ class DBHelper {
 
   //计算账户剩余总额(已完成)
   Future<double> accountBalance(int id) async {
+    print('正在计算$id 个账户的剩余金额');
     var dbClient = await db;
     var maps = await dbClient.rawQuery(
         "SELECT $columnMoney FROM $tableAccount where: $columnId = $id");
